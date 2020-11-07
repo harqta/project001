6abff88d4915b3c58e750c4d025d3e8d8f6d97eb
harqta <66869281+harqta@users.noreply.github.com>
2020-11-07 10:30:30 +0800

    updatd the nam

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br> Edited By Haroon</br>
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
